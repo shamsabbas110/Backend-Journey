bf312c1401c1fe929af3a4388eec70df30d7ad9a
Shams Abbas <shamsabbas795@gmail.com>
2026-03-05 02:20:57 +0530

    day-8 done

--- day-8/src/app.js
+++ day-8/src/app.js
@@ -3,7 +3,7 @@ const app=express();
 app.use(express.json());
 const noteModel=require("./models/notes.model");
 
-app.post("/notes",async(req,res)=>{
+app.post("/api/notes",async(req,res)=>{
     const{title,description}=req.body;
     const note=await noteModel.create({
         title,description
@@ -12,22 +12,26 @@ app.post("/notes",async(req,res)=>{
         message:"Note Created Successfully"
     })
 })
-app.get("/notes",async(req,res)=>{
+app.get("/api/notes",async(req,res)=>{
     const note=await noteModel.find()
     res.status(200).json({
         message:"Note Fetched Successfully",
         note
     })
 })
-
-
-
-
-
-
-
-
-
-
-
+app.delete("/api/notes/:id",async(req,res)=>{
+    const id = req.params.id;
+    await noteModel.findByIdAndDelete(id);
+    res.status(204).json({
+        message:"Note Deleted Successfully"
+    })
+})
+app.patch("/api/notes/:id",async(req,res)=>{
+    const id= req.params.id;
+    const{description}=req.body;
+    await noteModel.findByIdAndUpdate(id,{description})
+    res.status(200).json({
+        message:"Description Updated Successfully"
+    })
+})
 module.exports=app;
